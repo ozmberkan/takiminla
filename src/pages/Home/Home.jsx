3870b6342dcff7a694040a8f2431a08d4f47dc13
ozmberkan <ozmberkan@gmail.com>
2024-11-08 10:58:52 +0300

    Arka plan için güncelleme.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,19 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className=" bg-mainBg h-full flex-grow rounded-t-[70px] flex justify-center items-end relative">
-      <div className="container mx-auto h-[400px] flex justify-center items-center gap-6 ">
-        <button className="px-4 py-2 rounded-full bg-white text-sm">
-          Hemen Başla
-        </button>
-        <button className="px-4 py-2 rounded-full bg-white text-sm">
-          Hemen Başla
-        </button>
-        <button className="px-4 py-2 rounded-full bg-white text-sm">
-          Hemen Başla
-        </button>
-      </div>
-    </div>
+    <div className=" bg-mainBg h-full  flex-grow rounded-t-[70px] flex justify-center items-center "></div>
   );
 };
 
