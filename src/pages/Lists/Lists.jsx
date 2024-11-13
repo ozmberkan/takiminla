66045d2f49b0ba26a826f1b8bed9d3fdbcd25989
ozmberkan <ozmberkan@gmail.com>
2024-11-13 14:23:13 +0300

    Bildirim için ve liste için sayfalama tamamlandı.

--- src/pages/Lists/Lists.jsx
+++ src/pages/Lists/Lists.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { TbLayoutDistributeHorizontal } from "react-icons/tb";
+import { useEffect } from "react";
+import { TbListDetails } from "react-icons/tb";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllTeams } from "~/redux/slices/teamsSlice";
 
@@ -17,8 +17,8 @@ const Lists = () => {
   return (
     <div className="bg-mainBg bg-no-repeat bg-center bg-cover h-screen flex flex-col justify-start items-start relative overflow-hidden">
       <h1 className="container mx-auto text-3xl font-semibold text-zinc-800 mb-5 mt-12 border-b pb-5 flex gap-x-2 items-center">
-        <TbLayoutDistributeHorizontal />
-        İlanlarım
+        <TbListDetails />
+        Tüm İlanlar
       </h1>
       <div className="bg-white shadow-2xl container mx-auto rounded-xl p-12 flex flex-col gap-4">
         {teams.map((team) => (
--- src/pages/MyNotifications/MyNotifications.jsx
+++ src/pages/MyNotifications/MyNotifications.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { TbBell } from "react-icons/tb";
 
 const MyNotifications = () => {
-  return <div>MyNotifications</div>;
+  return (
+    <div className="bg-mainBg bg-no-repeat bg-center bg-cover h-screen flex flex-col justify-start items-start relative overflow-hidden">
+      <h1 className="container mx-auto text-3xl font-semibold text-zinc-800 mb-5 mt-12 border-b pb-5 flex gap-x-2 items-center">
+        <TbBell />
+        Bildirimlerim
+      </h1>
+      <div className="bg-white shadow-2xl container mx-auto rounded-xl p-12 flex flex-col gap-4">
+        Bildirim.
+      </div>
+    </div>
+  );
 };
 
 export default MyNotifications;
